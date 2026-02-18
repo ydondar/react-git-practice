d253e36914f60f1d7d77983416272489878fbf7c
Yusuf Dondar <y.dundar_01@hotmail.com>
2026-02-18 22:45:52 +0300

    initial commit from local

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [input, setInput] = useState('');
+  const [items, setItems] = useState(() => {
+    const saved = localStorage.getItem('todos');
+    return saved ? JSON.parse(saved) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(items));
+  }, [items]);
+
+  function handleAddItem() {
+    if (input.trim() === '') {  
+    alert('Boş bırakmayın');
+    return;
+    }
+
+    const alreadyExists = items.some(
+      (item) => item.text.toLowerCase() === input.toLowerCase()
+    );
+  
+    if (alreadyExists) {
+      alert('Bu eleman zaten listede ekli');
+      return;
+    }
+
+    const newItem = {
+      id : Date.now(),
+      text : input,
+      done: false,
+    };
+
+    setItems([...items, newItem]);
+    setInput('');
+
+  }
+
+  function handleDeleteItem(deleteId) {
+    const newItem = items.filter((item) => {
+      return item.id !== deleteId;
+    })
+    setItems(newItem);
+  }
+
+  function toggleItem(toggleId) {
+    const newItem = items.map((item) => {
+    if (toggleId === item.id) {
+    return {...item, done: !item.done}
+    }
+    return item;
+  });
+
+  setItems(newItem);
+  }
+
+  return (
+    <div>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={handleAddItem}>Ekle</button>
+    
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+        <span className = { item.done ? "todo-text done" : "todo-text" }>
+            {item.text}
+        </span>
+        <button onClick={() => toggleItem(item.id)}>
+        {item.done ? "Geri Al" : "Tamamlandı"}
+        </button>
+        <button onClick={() => handleDeleteItem(item.id)}>Sil</button> 
+        </li>
+        ))}
+      </ul>
+
+    </div>
+  );
+  
+}
+
+export default App;
